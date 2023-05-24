2a13470e0cec564761bf87bef6be80c5d16ec4af
zannatin-tazri <zannatintazrizan@gmail.com>
2023-05-25 03:00:53 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Footer/Footer';
+import Header from '../Pages/Shared/Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
