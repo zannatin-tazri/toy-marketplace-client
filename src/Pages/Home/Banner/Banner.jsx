bb0597355ed643b46b2ef60438c9fceca9215cd3
zannatin-tazri <zannatintazrizan@gmail.com>
2023-05-25 23:58:58 +0600

    gallery title and sub title

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,7 +5,8 @@ import img4 from '../../../../public/img14.jpeg';
 import img5 from '../../../../public/panda2.webp';
 const Banner = () => {
     return (
-        <div className='flex text-center m-6 p-8 gap-6 md:flex-none'>
+       <div>
+         <div className='flex text-center m-6 p-8 gap-6 md:flex-none'>
             <div className=" carousel rounded-box max-w-96 max-h-96 flex-1 w-64 ">
                 <div className="carousel-item ">
                     <img  src={img1} alt="" />
@@ -22,7 +23,9 @@ const Banner = () => {
                 <div className="carousel-item">
                     <img src={img5} alt="" />
                 </div>
+                
             </div>
+
             <div className='flex-1 w-32 mt-16'>
                 <h2 className='font-bold text-4xl mb-8'>Furry Friends Bazaar: A Haven for Animal Lovers!</h2>
                 <p>Delight Your Furry Friends with Irresistible Toys and Treats. <br />
@@ -31,6 +34,8 @@ const Banner = () => {
                     delightful assortment that will bring tail-wagging happiness to your furry companions.</p>
             </div>
         </div>
+        <small className='ms-24 ps-6'>Swipe to see toys</small>
+       </div>
     );
 };
 
--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -13,21 +13,27 @@ import img12 from '../../../../public/panda2.webp'
 
 const Gallery = () => {
     return (
-        <div className='p-5'>
+        <div className='p-6'>
+            < hr />
+            <h1 className='font-bold text-6xl text-center mb-6 mt-8'>Toy Gallery</h1>
+            <p className=' text-sm text-center mb-14 '>From engaging toys to delectable treats, 
+                we offer a delightful assortment that will bring tail-wagging happiness to your furry companions.
+                From engaging toys to delectable treats, 
+                we offer a delightful assortment that will bring tail-wagging happiness to your furry companions.</p>
             <div className="grid grid-cols-3 gap-6 ">
                 
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img1} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80 ' src={img2} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80'src={img12} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img4} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img5} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img6} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img7} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img8} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img9} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img10} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img3} alt="" />
-                  <img className='border border-indigo-600 rounded h-80 w-80' src={img11} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img1} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80 ' src={img2} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80'src={img12} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img4} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img5} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img6} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img7} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img8} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img9} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img10} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img3} alt="" />
+                  <img className='border border-yellow-300 rounded h-80 w-80' src={img11} alt="" />
 
                 
             </div>
