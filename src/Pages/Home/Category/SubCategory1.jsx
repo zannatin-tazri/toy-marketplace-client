5959f867438912f1775377d6b873ee02d0b6b05e
zannatin-tazri <zannatintazrizan@gmail.com>
2023-05-27 01:41:13 +0600

    teddy cards done

--- src/Pages/Home/Category/SubCategory1.jsx
+++ src/Pages/Home/Category/SubCategory1.jsx
@@ -1,19 +1,38 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { useEffect, useState } from "react";
 
 
 const SubCategory1 = () => {
-    const [subCategories1, setSubCategories ]=useState([]);
+    const [subCategories1, setSubCategories1] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/teddyBear')
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-        .catch(error=>console.log(error));
-    },[])
+            .then(res => res.json())
+            .then(data => setSubCategories1(data))
+            .catch(error => console.log(error));
+    }, [])
     return (
         <div>
-            <h3>hi1</h3>
+            <h1 className="text-center text-4xl font-bold">Teddy Bears</h1>
+            {
+                subCategories1.map(subCategory1 => <div className="grid grid-row-3 mt-5"
+                    key={subCategory1.id}>
+                    <div className="card card-side bg-base-100 shadow-xl">
+                        <figure><img className="w-80 h-64 rounded p-5" src={subCategory1.Picture} alt="Movie" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title text-2xl font-bold">{subCategory1.Name}</h2>
+                            <p className="text-xl font-semibold">Price : {subCategory1.Price}</p>
+                            <p className="text-md decoration-gray-300 font-semibold">Ratings : {subCategory1.Rating}</p>
+                            <p><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/></p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-outline btn-warning">View Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
